06b6822b7109cd2a6cc1794362be167a4125d8f9
freddyother <freddyother@hotmail.com>
2024-07-30 14:38:58 +0100

    test

---
+++ src/main/java/com/realestate/rentalandsales/RentalandsalesApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.rentalandsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalandsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalandsalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realestate/rentalandsales/RentalandsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.rentalandsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalandsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
